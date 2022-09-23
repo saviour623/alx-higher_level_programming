2c25154ce1d8c1bafe078d271f81114ae27eb31e
saviour623 <saviourme33@gmail.com>
2022-09-23 16:56:51 +0000

    Python: if,else and function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ elif x < 6 and x != 0:
     text = "and is less than 6 and not 0"
 else:
     text = "and is greater than 5"
-    print(f"Last digit of {number} is {x:d} {text}")
+print(f"Last digit of {number:s} is {x:d} {text:s}")
