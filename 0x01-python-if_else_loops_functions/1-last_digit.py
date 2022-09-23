93d697376a30971f3cf33585c78f69da5406d2ff
saviour623 <saviourme33@gmail.com>
2022-09-23 16:51:57 +0000

    Python: if,else and function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 number = str(number)
-x = abs(int(number[-1]))
+x = int(number[-1])
 if number[0] == '-':
     x = x * -1
-if int(number[-1]) == 0:
+if x == 0:
     text = "and is 0"
-elif x < 6 and x:
+elif x < 6 and x != 0:
     text = "and is less than 6 and not 0"
 else:
     text = "and is greater than 5"
-print(f"Last digit of {number} is {x:d} {text}")
+    print(f"Last digit of {number} is {x:d} {text}")
