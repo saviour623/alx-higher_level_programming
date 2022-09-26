e7bea192c573ddb61774d1d85a28c008e71cf7cd
saviour623 <saviourme33@gmail.com>
2022-09-26 16:58:47 +0000

    Python: if,else and function

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     if i <= 9:
-        print(0, end="")
-    print(i, end="")
+        print("{:d}".format(0), end="")
+    print("{:d}".format(i), end="")
     if i != 98:
         print(", ", end="")
 print()
