ea43f90007662a6943452eb1f0eaeb78035d38e2
saviour623 <saviourme33@gmail.com>
2022-09-26 16:39:41 +0000

    Python: if,else and function

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(1, 99):
+    print("{a:d} = 0x{b:x}".format(a=i, b=i))
+          
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str1 = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            str1 += chr(ord(i) - 32)
+    return str1
+
