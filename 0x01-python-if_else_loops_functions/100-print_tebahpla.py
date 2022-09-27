6f2b70cdff7974889b78b0bfe7c0dba059b1d4c1
saviour623 <saviourme33@gmail.com>
2022-09-27 11:27:01 +0000

    Python: if,else and function

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 i = 122
 while i >= 97:
-    print("{:s}".format(chr(i)), end="");
-    if (i - 1) >= 97:
-        i = i - 1
-        print("{:s}".format(chr(i - 32)), end="")
-
-    i = i - 1
+        print("{:s}{:s}".format(chr(i)), chr((i - 1) - 32), end="")
+    i = i - 2
