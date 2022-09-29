cdf9ba796f9193c5714281192f368920dcf514be
saviour623 <saviourme33@gmail.com>
2022-09-29 07:55:16 +0000

    python modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 from sys import argv
-def main(argv):
-    print(argv)
 
-main(argv)
+if __name__ == '__main__':
+    def main(argv):
+        a_len = len(argv) - 1
+        if a_len  == 1:
+            print("{:d} argument".format(a_len))
+        else:
+             print("{:d} arguments".format(a_len))
+
+        for i in range(1, len(argv)):
+            s = ":"
+            print("{:d}".format(i), end="")
+            if argv[i]:
+                print("{:s} {:s}".format(s, argv[i]))
+            else:
+                s = "."
+                print("{:s}".format(s))
+    main(argv)
