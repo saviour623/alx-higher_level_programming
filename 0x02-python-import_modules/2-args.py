6fdecb57228fb2f30c72dbf0b306eb4493d1549f
saviour623 <saviourme33@gmail.com>
2022-09-29 11:13:52 +0000

    python modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,19 @@
 from sys import argv
 
 if __name__ == '__main__':
-        p = "."
-        s = ":"
-        sp = ": "
-        ar1 = "argument"
-        ar2 = "arguments"
-        a_len = len(argv) - 1
-        if a_len  == 1:
-            print("{:d} {:s}{:s}".format(a_len, ar1, s))
-        elif a_len == 0:
-            print("{:d} {:s}{:s}".format(a_len, ar2, p))
-        else:
-            print("{:d} {:s}{:s}".format(a_len, ar2, s))
-        for i in range(1, len(argv)):
-            print("{:d}{:s}{:s}".format(i, sp, argv[i]))
+    p = "."
+    s = ":"
+    sp = ": "
+    ar1 = "argument"
+    ar2 = "arguments"
+    a_len = len(argv) - 1
+
+    if a_len == 1:
+        print("{:d} {:s}{:s}".format(a_len, ar1, s))
+    elif a_len == 0:
+        print("{:d} {:s}{:s}".format(a_len, ar2, p))
+    else:
+        print("{:d} {:s}{:s}".format(a_len, ar2, s))
+
+    for i in range(1, len(argv)):
+        print("{:d}{:s}{:s}".format(i, sp, argv[i]))
