da0ed4fd19ff6792176da8c11968612cb8a3aacb
saviour623 <saviourme33@gmail.com>
2022-09-29 08:24:52 +0000

    python modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,13 +8,11 @@ if __name__ == '__main__':
         ar1 = "argument"
         ar2 = "arguments"
         a_len = len(argv) - 1
-
         if a_len  == 1:
             print("{:d} {:s}{:s}".format(a_len, ar1, s))
         elif a_len == 0:
             print("{:d} {:s}{:s}".format(a_len, ar2, p))
         else:
             print("{:d} {:s}{:s}".format(a_len, ar2, s))
-
         for i in range(1, len(argv)):
             print("{:d}{:s}{:s}".format(i, sp, argv[i]))
