145714cb71e8e1b5031e690f6598952f18e8887d
saviour623 <saviourme33@gmail.com>
2022-09-30 09:39:16 +0000

    python modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
-import hidden_4
-
 if __name__ == '__main__':
+    import sys
+    import hidden_4
     def main():
         for i in div(hidden_4):
             if i[:2] != '__':
